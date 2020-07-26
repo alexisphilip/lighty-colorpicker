03b900988f4276ab9affaafc182f213856f5068b
Alexis Philip <alexis.philip13@gmail.com>
2020-07-26 14:43:38 +0200

    [ADD] hue color palette algorithm + adaptive resolution and height.

--- v3/main.js
+++ v3/main.js
@@ -12,6 +12,8 @@
  * Blackness  (percentage of black)       0-100%
  *
  * TODO: see if this helps https://jonneal.dev/convert-colors/global.html#hex2hsv
+ * https://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
+ * https://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
  */
 
 // var rgb = [85,114,156];
@@ -29,30 +31,79 @@
 // console.log(rgbToCmyk(rgb[0], rgb[1], rgb[2]));
 
 var classicPaletteCanvas = document.querySelector("#classicPalette"),
-    classicPaletteCtx = classicPaletteCanvas.getContext("2d")
+    huePaletteCanvas = document.querySelector("#huePalette"),
+    classicPaletteCtx = classicPaletteCanvas.getContext("2d"),
+    huePaletteCtx = huePaletteCanvas.getContext("2d");
 
 // Color picker set up.
-var hue = [255, 0, 0],
-    width = 400,
-    height = 300,
-    resolution_width = 40,
-    resolution_height = 30,
-    columns = Math.round(width / resolution_width),
-    rows = Math.round(height / resolution_height);
+var classicPalette = {
+        hue: [255, 0, 0],
+        width: 400,
+        height: 300,
+        resolution_width: 8,
+        resolution_height: 6
+    },
+    huePalette = {
+        fade: {
+            r: 255,
+            g: 255,
+            b: 255
+        },
+        width: 400,
+        height: 300,
+        resolution_width: 8,
+        resolution_height: 6
+    };
 
 drawClassicPalette();
+drawHuePalette();
+
+function drawHuePalette() {
+
+    var columns = Math.round(huePalette.width / huePalette.resolution_width),
+        rows = Math.round(huePalette.height / huePalette.resolution_height);
+
+    huePaletteCtx.canvas.width = huePalette.width;
+    huePaletteCtx.canvas.height = huePalette.height;
+
+    var hue_values = getHue(columns),
+        fade_r = huePalette.fade.r, // The bottom fading color.
+        fade_g = huePalette.fade.g,
+        fade_b = huePalette.fade.b;
+
+    for (let i = 0; i < rows; i++) {
+
+        for (let j = 0; j < columns; j++) {
+
+            // Sets the hue color.
+            var r = hue_values[j][0],
+                g = hue_values[j][1],
+                b = hue_values[j][2];
+
+            // Adds the bottom color fade to it.
+            r += (fade_r - r) / ((rows - 1) / i);
+            g += (fade_g - g) / ((rows - 1) / i);
+            b += (fade_b - b) / ((rows - 1) / i);
+
+            huePaletteCtx.fillStyle = "#" + rgbToHex(Math.round(r), Math.round(g), Math.round(b));
+            huePaletteCtx.fillRect(j * huePalette.resolution_width, i * huePalette.resolution_height, huePalette.resolution_width, huePalette.resolution_height);
+        }
+    }
+}
 
 function drawClassicPalette() {
 
-    var white_r = 255, // White color (color on the left).
+    var columns = Math.round(classicPalette.width / classicPalette.resolution_width),
+        rows = Math.round(classicPalette.height / classicPalette.resolution_height),
+        white_r = 255, // White color (color on the left).
         white_g = 255,
         white_b = 255,
-        hue_r = hue[0], // Hue color (color on the right).
-        hue_g = hue[1],
-        hue_b = hue[2];
+        hue_r = classicPalette.hue[0], // Hue color (color on the right).
+        hue_g = classicPalette.hue[1],
+        hue_b = classicPalette.hue[2];
 
-    classicPaletteCtx.canvas.width = width;
-    classicPaletteCtx.canvas.height = height;
+    classicPaletteCtx.canvas.width = classicPalette.width;
+    classicPaletteCtx.canvas.height = classicPalette.height;
 
     // For each rows.
     for (let i = 0; i < rows; i++) {
@@ -65,7 +116,7 @@ function drawClassicPalette() {
         for (let j = 0; j < columns; j++) {
 
             classicPaletteCtx.fillStyle = "#" + rgbToHex(Math.round(red), Math.round(green), Math.round(blue));
-            classicPaletteCtx.fillRect(j * resolution_width, i * resolution_height, resolution_width, resolution_height);
+            classicPaletteCtx.fillRect(j * classicPalette.resolution_width, i * classicPalette.resolution_height, classicPalette.resolution_width, classicPalette.resolution_height);
 
             // Calculates the needed color from white (color on the left) and the hue (color on the right).
             red -= (white_r - hue_r) / (columns - 1);
@@ -87,9 +138,9 @@ function drawClassicPalette() {
         white_b -= 255 / (rows - 1);
 
         // Subtracts hue color (color on the right).
-        hue_r -= hue[0] / (rows - 1);
-        hue_g -= hue[1] / (rows - 1);
-        hue_b -= hue[2] / (rows - 1);
+        hue_r -= classicPalette.hue[0] / (rows - 1);
+        hue_g -= classicPalette.hue[1] / (rows - 1);
+        hue_b -= classicPalette.hue[2] / (rows - 1);
 
         // If values are smaller than 0 (is this a JS float bug?).
         // if (white_r < 0) white_r = 0;
@@ -99,4 +150,105 @@ function drawClassicPalette() {
         // if (hue_g < 0) hue_g = 0;
         // if (hue_b < 0) hue_b = 0;
     }
+}
+
+
+function getHue(steps) {
+    var r = 255,
+        g = 0,
+        b = 0,
+        values = [[r, g, b]];
+
+    var total_bits = 1530,
+        bits = total_bits / (steps - 1);
+
+    var round_steps = 1;
+
+    for (let i = 0; i < steps - 1; i++) {
+        var remaining = 0;
+        if (round_steps === 1) {
+            g += bits;
+            if (g > 255) {
+                remaining = g - 255;
+                g = 255;
+                round_steps++;
+            } else {
+                remaining = 0;
+                values.push([r, g, b]);
+            }
+        }
+        if (round_steps === 2) {
+            if (remaining) r -= remaining;
+            else r -= bits;
+            if (r < 0) {
+                remaining = Math.abs(r);
+                r = 0;
+                round_steps++;
+            } else {
+                remaining = 0;
+                values.push([r, g, b]);
+            }
+        }
+        if (round_steps === 3) {
+            if (remaining) b += remaining;
+            else b += bits;
+            if (b > 255) {
+                remaining = b - 255;
+                b = 255;
+                round_steps++;
+            } else {
+                remaining = 0;
+                values.push([r, g, b]);
+            }
+        }
+        if (round_steps === 4) {
+            if (remaining) g -= remaining;
+            else g -= bits;
+            if (g < 0) {
+                remaining = Math.abs(g);
+                g = 0;
+                round_steps++;
+            } else {
+                remaining = 0;
+                values.push([r, g, b]);
+            }
+        }
+        if (round_steps === 5) {
+            if (remaining) r += remaining;
+            else r += bits;
+            if (r > 255) {
+                remaining = r - 255;
+                r = 255;
+                round_steps++;
+            } else {
+                remaining = 0;
+                values.push([r, g, b]);
+            }
+        }
+        if (round_steps === 6) {
+            if (remaining) b -= remaining;
+            else b -= bits;
+            if (b < 0) {
+                remaining = Math.abs(b);
+                b = 0;
+                round_steps++;
+            } else {
+                remaining = 0;
+                values.push([r, g, b]);
+            }
+        }
+        // console.log(Math.round(r), Math.round(g), Math.round(b));
+
+        // Essayer avec des while() et remaining :
+        // var remaining = 0;
+        // while (g < 255);
+        // while (r > 0);
+    }
+
+    // TODO: when asking for 100 steps, returns only 99
+    if (values.length !== steps) {
+        values.push([255, 0, 0]);
+    }
+
+    return values;
 }
